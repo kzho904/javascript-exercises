e36ee44f72564e5f0b9f76c9fde98b95e3bcc123
Katie Zhou <katiezh13533@gmail.com>
2026-05-19 22:24:13 +1200

    08

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (text, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    let result = "";
+    for (i = 0; i < num; i++) {
+      result += text;
+    }
+    return result;
+  }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
