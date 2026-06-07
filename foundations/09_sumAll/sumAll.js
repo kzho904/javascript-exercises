a456bc0e5f8992e38f2dfee906a8c0fa61eb37e8
Katie Zhou <katiezh13533@gmail.com>
2026-06-07 18:57:04 +1200

    09 done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  let total = 0;
+  if (min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)) {
+    return "ERROR";
+  }
 
+  if (min > max) {
+    let temp = min;
+    min = max;
+    max = temp;
+  }
+
+  for (let i = min; i <= max; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
