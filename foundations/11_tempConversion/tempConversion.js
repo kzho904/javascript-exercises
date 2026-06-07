f8c15fb04d1c1eeeb60d8ca1b3eaabadc383043c
Katie Zhou <katiezh13533@gmail.com>
2026-06-07 19:27:42 +1200

    11 done

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheit) {
+  let celsius = (farenheit - 32) * (5 / 9);
+  let rounded = Math.round(celsius * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let farenheit = (celsius * 9) / 5 + 32;
+  let rounded = Math.round(farenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
