1b2856b6794324d25dbbb6d52089f322ec310704
Katie Zhou <katiezh13533@gmail.com>
2026-06-07 19:14:38 +1200

    10 done

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  const isDivisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isDivisibleByFourHundred = year % 400 === 0;
+  if (isDivisibleByFour && (!isCentury || isDivisibleByFourHundred)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
