eb71351e0dba80e2a55c8e9e82b767ccd5f07eba
Katie Zhou <katiezh13533@gmail.com>
2026-08-20 22:18:02 +1200

    16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  oldest = people.reduce((oldest, current) => {
+    const currentYear = new Date().getFullYear();
+    let age;
+    let maxAge;
+    if (oldest == 0) {
+      return current;
+    }
+    if (current.yearOfDeath === undefined) {
+      age = currentYear - current.yearOfBirth;
+    } else {
+      age = current.yearOfDeath - current.yearOfBirth;
+    }
+    if (oldest.yearOfDeath === undefined) {
+      maxAge = currentYear - oldest.yearOfBirth;
+    } else {
+      maxAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    }
+    if (age > maxAge) {
+      return current;
+    }
+    return oldest;
+  }, 0)
+  return oldest
 };
 
 // Do not edit below this line
